c53e03296e62decab630b67d8ee6bba588c6afd8
lux <hilalmahdi11@gmail.com>
2023-07-10 13:08:11 +0700

    typo

--- hapus_LP.php
+++ hapus_LP.php
@@ -2,7 +2,7 @@
 include "koneksi.php";
 $Nama_barang = $_GET['Nama_barang'];
 
-$proses = mysqli_query($koneksi, "DELETE FROM laporan_Penjualan WHERE Nama_barang = '$Nama_barang'")
+$proses = mysqli_query($koneksi, "DELETE FROM laporan_penjualan WHERE Nama_barang = '$Nama_barang'")
     or die(mysqli_error($koneksi));
 
 if ($proses) {
